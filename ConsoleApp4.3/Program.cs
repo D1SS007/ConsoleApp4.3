51775119d3e531ed35272a5bdc2202930e5e2942
D1SS007 <101556566+D1SS007@users.noreply.github.com>
2022-04-10 22:16:06 +0300

    Update Program.cs

--- ConsoleApp4.3/Program.cs
+++ ConsoleApp4.3/Program.cs
@@ -1,29 +1,38 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp4._3
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            bool isConverted = true;
+        {            
+            TryParsing();           
+        }
 
-            while(isConverted)
+        static int TryParsing()
+        {
+            bool isNotConverted = true;
+
+            int value=0;
+
+            while (isNotConverted)
             {
                 Console.WriteLine("Ведите число");
 
                 string userInput = Console.ReadLine();
 
-                if (Int32.TryParse(userInput, out int value))
+                if (Int32.TryParse(userInput, out value))
                 {
-                    Console.WriteLine("Число переобразовано в : " + value);
-                    isConverted = false;
+                    Console.WriteLine("Число преобразовано в : " + value);
+                    
+                    isNotConverted = false;                    
                 }
                 else
                 {
                     Console.WriteLine($"Невозможно преобразовать {userInput} в число с типом int!");
                 }
-            }            
+            }
+            return value;
         }
     }
 }
