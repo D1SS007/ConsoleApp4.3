d8a603440e1aeeb19a0d74d61efec35767190160
Alex Zverev <lordzver007@yandex.ru>
2022-04-10 21:10:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isConverted = true;
+
+            while(isConverted)
+            {
+                Console.WriteLine("Ведите число");
+
+                string userInput = Console.ReadLine();
+
+                if (Int32.TryParse(userInput, out int value))
+                {
+                    Console.WriteLine("Число переобразовано в : " + value);
+                    isConverted = false;
+                }
+                else
+                {
+                    Console.WriteLine($"Невозможно преобразовать {userInput} в число с типом int!");
+                }
+            }            
+        }
+    }
+}
